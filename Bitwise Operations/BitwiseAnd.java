63e74428bd64666e1b5dca20f8924e05823206ab
JainAtishya <atishyajain62149@gmail.com>
2026-07-29 21:47:29 +0530

    implemented bitwise and operator using bitwise not and bitwise or operator

---
+++ Bitwise Operations/BitwiseAnd.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+
+class BitwiseAnd {
+
+    public static int bitAnd(int x, int y) {
+        return ~((~x) | (~y));
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(bitAnd(num1, num2));
+
+    }
+
+}
+
+
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
