a9e640cedbf30c4dad1dcce167232cd0b28a23b1
JainAtishya <atishyajain62149@gmail.com>
2026-08-03 16:04:03 +0530

    implemented logical right shift using bitwise operator

---
+++ Bitwise Operations/rightShift.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int logicalShift(int a, int n);
+int main(void)
+{
+    int a, n;
+    scanf("%d %d", &a, &n);
+
+    int ans = logicalShift(a, n);
+    printf("%d", ans);
+    return 0;
+}
+
+int logicalShift(int a, int n)
+{
+    int mask = ~(((1 << 31) >> n) << 1);
+    return (a >> n) & mask;
+}
