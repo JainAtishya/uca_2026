918bba065e29b807bcab3973373871c289dfa2c6
JainAtishya <atishyajain62149@gmail.com>
2026-08-11 14:42:19 +0530

    implemented fibonacci using generator function

---
+++ webdev/generator-function/fibonacci.js
@@ -0,0 +1,22 @@
+function* fibonacci() {
+    let a = 0;
+    let b = 1;
+
+    while (true) {
+        yield a;
+
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+
+const fibGen = fibonacci();
+
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
+console.log(fibGen.next().value);
